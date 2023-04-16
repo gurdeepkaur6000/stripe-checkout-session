7c95a61bb98a101af06bf344821a288a2f897e87
gurdeepkaur6000 <gurdeep.kaur6000@gmail.com>
2023-04-16 20:36:27 +0530

    added how to create tax rates and checkout session with amount and tax rates

--- stripe-checkout.php
+++ stripe-checkout.php
@@ -3,43 +3,43 @@
 // Set your API secret key
 $api_key = 'test key';
 
-// Set the endpoint URL
-// $url = 'https://api.stripe.com/v1/tax_rates';
+Set the endpoint URL
+$url = 'https://api.stripe.com/v1/tax_rates';
 
-// // Set the request headers
-// $headers = [
-//     'Authorization: Bearer ' . $api_key,
-//     'Content-Type: application/x-www-form-urlencoded',
-// ];
+// Set the request headers
+$headers = [
+    'Authorization: Bearer ' . $api_key,
+    'Content-Type: application/x-www-form-urlencoded',
+];
 
-// // Set the request data
-// $data = http_build_query([
-//     'display_name' => 'Sales Tax',
-//     'description' => 'Sales tax for example state',
-//     'jurisdiction' => 'IN',
-//     'percentage' => 18.0,
-//     'inclusive' => 'false', // set the parameter to a valid boolean value
-// ]);
+// Set the request data
+$data = http_build_query([
+    'display_name' => 'Sales Tax',
+    'description' => 'Sales tax for example state',
+    'jurisdiction' => 'IN',
+    'percentage' => 18.0,
+    'inclusive' => 'false', // set the parameter to a valid boolean value
+]);
 
-// // Set the cURL options
-// $ch = curl_init();
-// curl_setopt($ch, CURLOPT_URL, $url);
-// curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-// curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-// curl_setopt($ch, CURLOPT_POST, true);
-// curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+// Set the cURL options
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
 
-// // Execute the request
-// $response = curl_exec($ch);
+// Execute the request
+$response = curl_exec($ch);
 
-// // Close the cURL session
-// curl_close($ch);
+// Close the cURL session
+curl_close($ch);
 
-// // Parse the response as JSON
-// $tax_rate = json_decode($response, true);
+// Parse the response as JSON
+$tax_rate = json_decode($response, true);
 
 
-// echo "<pre>"; print_r($tax_rate);
+echo "<pre>"; print_r($tax_rate);
 // die;
 ////////////////////////////////////////////////////////////////////////
 // Set your API secret key
